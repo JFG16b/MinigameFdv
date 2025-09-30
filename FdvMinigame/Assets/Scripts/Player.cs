afcd1b89c8cd5799d41163cdaf895f509cfac4fb
JFG16b <javierferrerga16@gmail.com>
2025-09-30 20:59:40 +0200

    movimiento

--- FdvMinigame/Assets/Scripts/Player.cs
+++ FdvMinigame/Assets/Scripts/Player.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class jugador : MonoBehaviour
 {
-    private 
+
+    public float thrustForce = 5f;
+    public float rotationSpeed = 10f;
+
+    private Rigidbody _rigid;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float rotation = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float thrust = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        Vector3 thrustDirection = transform.right;
+
+        _rigid.AddForce(thrustDirection * thrust * thrustForce);
+
+        transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
     }
 }
